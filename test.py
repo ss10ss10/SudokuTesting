c642c753b491ca396cd68f70116033fecb09ed29
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-29 23:43:28 +0500

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+option = webdriver.ChromeOptions()
+option.add_argument("--headless")
+driver = webdriver.Chrome(
+    executable_path="/home/sulaiman/Python/selenium/chromedriver", chrome_options=option
+)
+driver.get("http://localhost:3000/")
+
+total_buttons = driver.find_elements(By.CLASS_NAME, "button")
+print(len(total_buttons))
